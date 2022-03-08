94d51e1db7df8af43d51ba732cb4ee1b6d03b61e
pauline-wim <linette.wim@hotmail.com>
2022-03-08 14:57:30 +0100

    setup project => create variable, middleware debug, get home, listen port 8000

--- server.js
+++ server.js
@@ -1,4 +1,49 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var app = express();
+var PORT = 8000;
 
-app.listen(8000, () => console.log("Listening..."));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("debug");
+  next();
+});
+
+const superheroes = [
+  {
+    name: "Iron Man",
+    power: ["money"],
+    color: "red",
+    isAlive: true,
+    age: 46,
+    image:
+      "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart",
+  },
+  {
+    name: "Thor",
+    power: ["electricity", "worthy"],
+    color: "blue",
+    isAlive: true,
+    age: 300,
+    image:
+      "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg",
+  },
+  {
+    name: "Daredevil",
+    power: ["blind"],
+    color: "red",
+    isAlive: false,
+    age: 30,
+    image:
+      "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Heroes");
+});
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
